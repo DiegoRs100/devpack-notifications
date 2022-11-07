b305fd419c24edbbb60c8c74bc9ad6e4d087fc99
Diego Rocha <diego.rocha@sitemercado.com.br>
2022-11-07 17:56:23 -0300

    Library creation

---
+++ src/Devpack.Notifications/Notifier/INotifier.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Devpack.Notifications.Notifier
+{
+    public interface INotifier
+    {
+        bool HasNotifications { get; }
+        IReadOnlyCollection<Notification> GetNotifications();
+        Task Notify(string message, HttpStatusCode notificationType = HttpStatusCode.UnprocessableEntity);
+        Task Notify(string errorCode, string message, HttpStatusCode notificationType = HttpStatusCode.UnprocessableEntity);
+        Task Notify(string errorCode, string field, string message, HttpStatusCode notificationType = HttpStatusCode.UnprocessableEntity);
+        Task Notify(ValidationResult validationResult, HttpStatusCode notificationType = HttpStatusCode.UnprocessableEntity);
+        JsonResult GetAsJsonResult();
+    }
+}
